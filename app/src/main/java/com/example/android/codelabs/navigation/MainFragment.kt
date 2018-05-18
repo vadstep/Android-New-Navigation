382a0e3a5ba07842f684a7fbd33b3610d9456c90
vadimsgmedical <vadims@gmedinnovations.com>
2018-05-18 10:59:43 +0300

    start
    added kotlin extentions

--- app/src/main/java/com/example/android/codelabs/navigation/MainFragment.kt
+++ app/src/main/java/com/example/android/codelabs/navigation/MainFragment.kt
@@ -18,11 +18,10 @@ package com.example.android.codelabs.navigation
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.navigation.NavOptions
+import androidx.navigation.Navigation.findNavController
+import kotlinx.android.synthetic.main.main_fragment.*
 
 /**
  * Fragment used to show how to navigate to another destination
@@ -37,24 +36,18 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        //TODO STEP 5 - Set an OnClickListener, using Navigation.createNavigateOnClickListener()
-//        view.findViewById<Button>(R.id.navigate_dest_bt)?.setOnClickListener(
-//            Navigation.createNavigateOnClickListener(R.id.flow_step_one, null)
-//        )
-        //TODO ENDSTEP 5
 
         //TODO STEP 6 - Set NavOptions
 
-//        val options = NavOptions.Builder()
-//            .setEnterAnim(R.anim.slide_in_right)
-//            .setExitAnim(R.anim.slide_out_left)
-//            .setPopEnterAnim(R.anim.slide_in_left)
-//            .setPopExitAnim(R.anim.slide_out_right)
-//            .build()
-//
-//        view.findViewById<Button>(R.id.navigate_dest_bt)?.setOnClickListener {
-//            findNavController(it).navigate(R.id.flow_step_one, null, options)
-//        }
+        val options = NavOptions.Builder()
+            .setEnterAnim(R.anim.slide_in_right)
+            .setExitAnim(R.anim.slide_out_left)
+            .setPopEnterAnim(R.anim.slide_in_left)
+            .setPopExitAnim(R.anim.slide_out_right)
+            .build()
+        navigate_dest_bt.setOnClickListener {
+            findNavController(it).navigate(R.id.flow_step_one, null, options)
+        }
         //TODO ENDSTEP 6
 
         //TODO STEP 7 - Update the OnClickListener to navigate using an action
